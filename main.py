66aa7a147561570dec33045a6c90baab53b762df
Pasi <148313514+PasiKami@users.noreply.github.com>
2024-09-06 17:22:51 +0800

    test

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import streamlit as st
 import time
 from sqlalchemy import create_engine
 
-engine1 = st.text_input('输入数据库1地址📝:', key="sql_create_table_name")
-engine2 = st.text_input('输入数据库2地址📝:', key="sql_create_table_name2")
+engine1 = st.text_input('输入内网数据库地址📝:', key="sql_create_table_name")
+engine2 = st.text_input('输入外网数据库地址📝:', key="sql_create_table_name2")
 
-if st.button("测试1"):
+if st.button("测试内网"):
     start_time = time.time()
     engine = create_engine(engine1)
     with engine.connect() as conn:
@@ -15,7 +15,7 @@ if st.button("测试1"):
     st.write(f"用时: {time.time() - start_time}")
     engine.dispose()
 
-if st.button("测试2"):
+if st.button("测试外网"):
     start_time = time.time()
     engine = create_engine(engine2)
     with engine.connect() as conn:
